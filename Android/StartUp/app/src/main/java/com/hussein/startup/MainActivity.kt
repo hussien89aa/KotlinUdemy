6101f7b0c5886b6d6187fb6aba5673366b1f78da
hussienalrubaye <hussien89aa@yahoo.com>
2017-05-21 20:17:27 -0400

    StartUp android

---
+++ Android/StartUp/app/src/main/java/com/hussein/startup/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hussein.startup
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import  kotlinx.android.synthetic.main.activity_main.*
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
