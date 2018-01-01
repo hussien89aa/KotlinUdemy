4c05caf7bc99b09970ab730ad3ee9009369ba3aa
hussien alrubaye <hussien89aa@yahoo.com>
2017-12-31 23:57:09 -0500

    comparable

--- Kotlin/CarClass.kt
+++ Kotlin/CarClass.kt
@@ -1,45 +1,37 @@
+/**
+ * Created by hussienalrubaye on 12/31/17.
+ */
 
-class  Car(Type:String, Model:Int, Price:Double,
-           MilesDrive:Int, Owner:String){
-     var Owner:String?=null
-     var Price:Double?=null
-     var MilesDrive:Int?=null
-     var  Model:Int?=null
-     var Type:String?=null
-    init {
-        println("Type:$Type")
-        println("Model:$Model")
-        println("Price:$Price")
-        println("MilesDrive:$MilesDrive")
-        println("Owner:$Owner")
-        this.Owner=Owner
-        this.Price=Price
-        this.MilesDrive=MilesDrive
-        this.Model=Model
-        this.Type=Type
+class Car(){
+    var type:String?=null
+    var model:Int?=null
+    var price:Double?=null
+    var milesDrive:Int?=null
+    var owner:String?=null
+    constructor( type:String, model:Int,  price:Double,
+                 milesDrive:Int, owner:String):this(){
+        this.type=type
+        this.model=model
+        this.price=price
+        this.milesDrive=milesDrive
+        this.owner=owner
+        println("constructor1")
 
     }
 
-    fun GetPrice():Double?{
-         return this.Price!! -( this.MilesDrive!!.toDouble() *10)
+    fun getCarPrice():Double {
+
+        return this.price!! - ( this.milesDrive!!.toDouble()*10)
     }
-
-    fun GetOwner(): String? {
-
-        return this.Owner;
-    }
-
-
 }
 
 
 
 fun  main(args:Array<String>){
-    var huCar= Car("BMW",2015,10000.0,105,"Hussein")
-    println("Get Owner:"+ huCar.GetOwner())
-    println("Get price:"+ huCar.GetPrice())
 
-    var jeCar= Car("Ka",2017,20000.0,1,"Jena")
-    println("Get Owner:"+ jeCar.GetOwner())
-    println("Get price:"+ jeCar.GetPrice())
+    val huCar= Car("BMW",2015,10000.0,105,"Hussein")
+    println("huCar:"+ huCar.getCarPrice())
+    val jeCar=Car("KA",2017,2000.0,1,"Jena")
+    println("jeCar:"+ jeCar.getCarPrice())
+
 }
