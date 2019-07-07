c79c56d9f96d081a7c2978cfe54cb433aecbda9d
hussien alrubaye <hussien89aa@yahoo.com>
2019-07-07 10:53:55 -0400

    migrate Zoo App to AndroidX

--- Android/ZooApp/StartUp/app/src/main/java/com/hussein/startup/AnimalInfo.kt
+++ Android/ZooApp/StartUp/app/src/main/java/com/hussein/startup/AnimalInfo.kt
@@ -1,6 +1,6 @@
 package com.hussein.startup
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_animal_info.*
 
@@ -10,7 +10,7 @@ class AnimalInfo : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animal_info)
 
-        val bundle:Bundle = intent.extras
+        val bundle:Bundle = intent.extras!!
         val name =bundle.getString("name")
         val des = bundle.getString("des")
         val image=bundle.getInt("image")
--- Android/ZooApp/StartUp/app/src/main/java/com/hussein/startup/MainActivity.kt
+++ Android/ZooApp/StartUp/app/src/main/java/com/hussein/startup/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.hussein.startup
 
 import android.content.Context
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
