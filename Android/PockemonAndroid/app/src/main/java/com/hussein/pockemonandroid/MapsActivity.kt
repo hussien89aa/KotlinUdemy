7c3165aa5a7de3747cb68fea4337c25ef92f6d3a
hussien alrubaye <hussien89aa@yahoo.com>
2019-07-07 10:03:15 -0400

    migrate PockemonApp to AndroidX

--- Android/PockemonAndroid/app/src/main/java/com/hussein/pockemonandroid/MapsActivity.kt
+++ Android/PockemonAndroid/app/src/main/java/com/hussein/pockemonandroid/MapsActivity.kt
@@ -6,9 +6,9 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Build
-import android.support.v4.app.FragmentActivity
+import androidx.fragment.app.FragmentActivity
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
+import androidx.core.app.ActivityCompat
 import android.util.Log
 import android.widget.Toast
 
