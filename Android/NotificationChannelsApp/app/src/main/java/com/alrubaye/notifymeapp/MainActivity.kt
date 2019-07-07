f2a667d07230660a03f1615e01d4eefec0757f35
hussien alrubaye <hussien89aa@yahoo.com>
2019-07-07 09:57:19 -0400

    migrate NotifyApp to AndroidX

--- Android/NotificationChannelsApp/app/src/main/java/com/alrubaye/notifymeapp/MainActivity.kt
+++ Android/NotificationChannelsApp/app/src/main/java/com/alrubaye/notifymeapp/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.alrubaye.notifymeapp
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
