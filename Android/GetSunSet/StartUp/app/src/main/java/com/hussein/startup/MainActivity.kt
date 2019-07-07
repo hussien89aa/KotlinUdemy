32287cb7757d409f8a2e1860e851ad03db206aea
hussien alrubaye <hussien89aa@yahoo.com>
2019-07-07 09:38:59 -0400

    migrate weatherApp to AndroidX

--- Android/GetSunSet/StartUp/app/src/main/java/com/hussein/startup/MainActivity.kt
+++ Android/GetSunSet/StartUp/app/src/main/java/com/hussein/startup/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.hussein.startup
 
 import android.os.AsyncTask
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import  kotlinx.android.synthetic.main.activity_main.*
