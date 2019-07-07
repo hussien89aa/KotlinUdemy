15d537a5449c8e799fdc929901182ee98a8ad54c
hussien alrubaye <hussien89aa@yahoo.com>
2019-07-07 10:12:51 -0400

    migrate sensor App to AndroidX

--- Android/sensors/light/StartUp/app/src/main/java/com/hussein/startup/MainActivity.kt
+++ Android/sensors/light/StartUp/app/src/main/java/com/hussein/startup/MainActivity.kt
@@ -6,7 +6,7 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.media.MediaPlayer
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import  kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity(),SensorEventListener {
--- Android/sensors/numbizz/StartUp/app/src/main/java/com/hussein/startup/MainActivity.kt
+++ Android/sensors/numbizz/StartUp/app/src/main/java/com/hussein/startup/MainActivity.kt
@@ -5,7 +5,7 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Vibrator
 import android.widget.Toast
