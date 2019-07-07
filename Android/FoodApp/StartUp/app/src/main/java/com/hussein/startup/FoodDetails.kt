db3ebf33ff80f03fc3dea9ffd709a5c3f005768a
hussien alrubaye <hussien89aa@yahoo.com>
2019-07-07 09:33:16 -0400

    migrate FoodApp to AndroidX

--- Android/FoodApp/StartUp/app/src/main/java/com/hussein/startup/FoodDetails.kt
+++ Android/FoodApp/StartUp/app/src/main/java/com/hussein/startup/FoodDetails.kt
@@ -1,6 +1,6 @@
 package com.hussein.startup
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_food_details.*
 
@@ -12,9 +12,9 @@ class FoodDetails : AppCompatActivity() {
         setContentView(R.layout.activity_food_details)
         val bundle=intent.extras
 
-        ivFoodImage.setImageResource(bundle.getInt("image"))
-        tvName.text = bundle.getString("name")
-        tvDetails.text = bundle.getString("des")
+        ivFoodImage.setImageResource(bundle!!.getInt("image"))
+        tvName.text = bundle!!.getString("name")
+        tvDetails.text = bundle!!.getString("des")
 
     }
 }
--- Android/FoodApp/StartUp/app/src/main/java/com/hussein/startup/MainActivity.kt
+++ Android/FoodApp/StartUp/app/src/main/java/com/hussein/startup/MainActivity.kt
@@ -2,7 +2,7 @@ package com.hussein.startup
 
 import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
